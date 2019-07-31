b0771b97149f0b2ab7f9fc23baab846921976227
axxeerick <42874373+axxeerick@users.noreply.github.com>
2019-07-30 20:35:27 -0400

    Add files via upload

---
+++ upload.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# $1='commit message'
+
+hugo
+
+cd public
+git add --all
+if [ "$1" != "" ] 
+    then
+        git commit -m "$1"
+    else
+        git commit -m "update"
+    fi
+git push 
+
+cd .. 
+
+git add --all 
+if [ "$1" != "" ] 
+    then
+        git commit -m "$1"
+    else
+        git commit -m "update"
+    fi
+git push 
+
+
+
